4a495bdd92b9d522d012408ea6675e123989a7b9
Nikita Kori <133592008+nikitakori@users.noreply.github.com>
2026-05-17 21:41:21 +0530

    Add files via upload

---
+++ Arraylist.java
@@ -0,0 +1,19 @@
+package ARRAY;
+import java.util.ArrayList;
+
+public class Arraylist {
+    public static void main(String[] args){
+        ArrayList<Integer> num = new ArrayList<Integer>();
+
+        //add elements in list
+
+        num.add(19);
+        num.add(16);
+        num.add(14);
+        num.add(13);
+        num.add(11);
+        num.add(133);
+
+        System.out.println(num);
+    }
+}
