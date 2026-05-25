e1401d95ea67bc46c9e308e3c61a91603fe0e6d3
Nikita Kori <133592008+nikitakori@users.noreply.github.com>
2026-05-25 20:06:59 +0530

    Add files via upload

---
+++ Recursion/src/FibobyFormula.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class FibobyFormula{
+    public static void main(String[] args){
+        for(int i=0; i<50; i++){
+            System.out.println(fiboFormula(i));
+        }
+    }
+
+    static int fiboFormula(int n){
+        return (int)((Math.pow(((1 + Math.sqrt(5))/2), n) - Math.pow(((1 - Math.sqrt(5))/2), n))/Math.sqrt(5));
+    }
+
+
+    static int fiboBase(int n) {
+        // base condition
+        if (n < 2) {
+            return n;
+        }
+        return fiboBase(n-1) + fiboBase(n-2);
+    }
+}
+
+//(Math.pow(((1 + Math.sqrt(5)))/2, n) - Math.pow(((1 - Math.sqrt(5)))/2, n))
