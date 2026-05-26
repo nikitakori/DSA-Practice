c72784a698ce14be95025aa54e5efdcf777883d4
Nikita Kori <133592008+nikitakori@users.noreply.github.com>
2026-05-26 19:21:38 +0530

    Add getBit method to check specific bit value

--- Bit Manipulation/src/BitManipulation.java
+++ Bit Manipulation/src/BitManipulation.java
@@ -1,8 +1,20 @@
 public class BitManipulation {
     public static void main(String[] args){
         System.out.println(isEven(338));
+        System.out.println(getBit(10,2));
     }
+    //check number is even or odd
     static boolean isEven(int n){
         return (n & 1)==1;
+    }
+    //check the bit is 1 or 0
+    static int getBit(int n,int i){
+//        return ((n & (1 << 2)) == 1 ? 1 : 0);
+        int bit_mask = 1<<i;
+        if((n & bit_mask ) == 0){
+            return 0;
+        }else {
+            return 1;
+        }
     }
 }
