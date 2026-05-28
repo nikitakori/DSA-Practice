1e9db38be94daafc041775a1b79d0db0edd400cf
Nikita Kori <133592008+nikitakori@users.noreply.github.com>
2026-05-29 00:14:54 +0530

    28-05-26

---
+++ String/src/LowerCase.java
@@ -0,0 +1,9 @@
+public class LowerCase {
+    public static void main(String[] args){
+        String s = "The Shining Star";
+        System.out.println(toLowerCase(s));
+    }
+    public static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
