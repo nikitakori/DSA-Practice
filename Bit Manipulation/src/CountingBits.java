d6f435365ac666e4d62c9b1dc84a1cf24aecada3
Nikita Kori <133592008+nikitakori@users.noreply.github.com>
2026-05-31 23:39:38 +0530

    add question link

--- Bit Manipulation/src/CountingBits.java
+++ Bit Manipulation/src/CountingBits.java
@@ -1,4 +1,5 @@
 class CountingBits {
+    //https://leetcode.com/problems/counting-bits/description/
     public int[] countBits(int n) {
         int[] ans = new int[n+1];
         for(int i=0; i<=n; i++){
