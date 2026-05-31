ffc618b775c6c53a020569a93456a36fc5c386c4
Nikita Kori <133592008+nikitakori@users.noreply.github.com>
2026-05-31 23:41:16 +0530

    add brute force method
    Added a method to count the number of 1 bits in each number from 0 to n.

--- Bit Manipulation/src/CountingBits.java
+++ Bit Manipulation/src/CountingBits.java
@@ -7,4 +7,20 @@ class CountingBits {
         }
         return ans;
     }
+    //basic thinking
+    public int[] countBits(int n) {
+        int[] ans = new int[n+1];
+        for(int i=0; i<=n; i++){
+            int num = i;
+            int count = 0;
+            while(num > 0){
+                if((num & 1) == 1){
+                   count++; 
+                }
+                num = num >> 1;
+            }
+            ans[i] = count;
+        }
+        return ans;
+    }
 }
